61be9bc3ffaa9ae9a3044c51c260fe70c1a695fd
Lacey Merryman <laceymerryman@Laceys-MacBook-Pro.local>
2025-09-27 10:23:45 -1000

    modify initial

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
 const mysql = require("mysql2");
 
-const config =
-  process.env.JAWSDB_URL ||
-  {
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    database: process.env.npm_package_config_DB_NAME,
-  };
-
-const db = mysql.createPool(config);
+const db = mysql.createPool(process.env.JAWSDB_URL || {
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  database: process.env.npm_package_config_DB_NAME,
+});
 
 module.exports = db.promise();
