8a1cba9418232190f14f37bc97b1a2d4208d6e6e
Lacey Merryman <laceymerryman@Laceys-MacBook-Pro.local>
2025-09-27 10:49:55 -1000

    Initial commit with server, routes, and images

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const apiRoutes = require('./api-routes')
-const app = express()
+// app.js
+const express = require('express');
+const path = require('path');
+const apiRoutes = require('./api-routes');
 
-app.use(express.json())
-app.use(express.static('public'))
-app.use('/api', apiRoutes)
+const app = express();
 
-module.exports = app
+// Middleware for parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use your API routes
+app.use('/', apiRoutes);
+
+module.exports = app;
--- db.js
+++ db.js
@@ -1,28 +1,11 @@
 const mysql = require("mysql2");
-const url = require("url");
 
-let config;
-
-if (process.env.JAWSDB_URL) {
-  const connectionUrl = url.parse(process.env.JAWSDB_URL);
-  const [user, password] = connectionUrl.auth.split(":");
-
-  config = {
-    connectionLimit: 10,
-    host: connectionUrl.hostname,
-    user: user,
-    password: password,
-    database: connectionUrl.pathname.replace("/", ""),
-  };
-} else {
-  config = {
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    database: process.env.npm_package_config_DB_NAME,
-  };
-}
-
+const config = process.env.JAWSDB_URL || {
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  database: process.env.npm_package_config_DB_NAME,
+};
 const db = mysql.createPool(config);
 
 module.exports = db.promise();
